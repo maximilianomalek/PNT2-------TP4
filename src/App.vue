c500bd496921f79d4b3f082ac11a3069ee2e0d67
maximilianomalek <malekmaximiliano@gmail.com>
2022-11-10 20:09:28 -0300

    trabajo practico 4 - Maximiliano Malek

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container-fluid mt-3">
+    <div class="jumbotron">
+      <h1>PNT2 - TP4</h1>
+      <br>
+  
+      <NavBar/>
+      <router-view></router-view>
+
+      <br>
+      <br>
+
+      <h2>Alumno: Maximiliano Malek</h2>
+  
+  
+    </div>
+  </div>
+  </template>
+  
+  <script>
+  
+  import NavBar from './components/NavBar.vue'
+  
+  
+  export default {
+    name: 'App',
+    components: {
+       NavBar
+    }
+  }
+  </script>
+  
+  <style>
+    h1,h2 {
+      color: rgb(174, 164, 158);
+    }
+    div .jumbotron{
+      background-color: rgb(42, 35, 35);
+    }
+
+    div .container-fluid{
+      background-color: blanchedalmond;
+    }
+
+  </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,68 @@
+<template>
+
+  <section class="src-components-navbar">
+    <nav id="navBar" class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <router-link to="/">
+        <a class="navbar-brand" href="#">Inicio</a>
+      </router-link>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <!-- navegacion binding -->
+          <li class="nav-item">
+            <router-link to="/formulario">
+              <a class="nav-link" href="#">Formulario</a>
+            </router-link>
+          </li>
+          <!-- navegacion estructura -->
+          <li class="nav-item">
+            <router-link to="/http-client">
+              <a class="nav-link" href="#">HttpClient</a>
+            </router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </section>
+
+</template>
+
+<script>
+
+  export default  {
+    name: 'src-components-navbar',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="css">
+a .navbar-brand:hover,.navbar-dark .navbar-nav .nav-link:hover{
+  color: brown;
+  transition: 1s;
+}
+
+#navBar{
+  background-color: rgb(61, 62, 63)!important;
+}
+
+</style>
